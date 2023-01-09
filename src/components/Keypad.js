dc8835e9cb1e2c1303aba12afd7f1e822fa19841
farhanfs008 <66518033+farhanfs008@users.noreply.github.com>
2023-01-09 16:01:59 +0700

    Update Keypad.js

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -4,7 +4,7 @@ export default function Keypad( {usedKeys} ) {
     const [letters, setLetters] = useState(null)
 
     useEffect(() => {
-        fetch('http://localhost:3001/letters')
+        fetch('https://farhanfs008.github.io/json-wordle/db.json')
         .then(res => res.json())
         .then(json => {
             setLetters(json)
